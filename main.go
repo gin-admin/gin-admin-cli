944c353cb272b6e77f3b7201ecdc9981e66a98fe
tiannianshou <tiannianshou@gmail.com>
2020-05-12 10:57:05 +0800

    Update version

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "gin-admin-cli"
 	app.Description = "GinAdmin辅助工具，提供创建项目、快速生成功能模块的功能"
-	app.Version = "3.0.0"
+	app.Version = "3.0.2"
 	app.Commands = []cli.Command{
 		cmd.NewCommand(),
 		cmd.GenerateCommand(),
